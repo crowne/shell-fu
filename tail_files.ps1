a71203355087dc19985e01270e47704ed634016c
Neil Crow <crowneil@gmail.com>
2021-12-02 20:37:02 +0200

    Create tail_files.ps1

---
+++ tail_files.ps1
@@ -0,0 +1,5 @@
+# The following command takes a directory listing of all sub-dirs containing files called *.txt
+# It writes the path of the file, then the last 5 lines of the file and then a new-line
+# The line breaks from the last 5 lines are not preserved.
+
+gci *.txt -recurse | ForEach-Object  { Write-Host "$_`n" + $(Get-Content $_ -tail 5) + "`n" }
