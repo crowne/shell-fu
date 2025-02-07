b86ae0746c03055934969c8e0bd556c681cde228
crowne <crow.neil@gmail.com>
2025-02-07 14:12:56 +0000

    improve install

--- powershell/install-shell-fu.ps1
+++ powershell/install-shell-fu.ps1
@@ -1,4 +1,13 @@
+function makeDir([string]$dir_name) {
+    if ( !( Test-Path $dir_name -PathType Container ) ) {
+        Write-Host "Creating $dir_name"
+        mkdir $dir_name
+    }
+}
+
 $profile_dir = Split-Path -Path $Profile -Parent
+makeDir($profile_dir)
 $modules_home = Join-Path -Path $profile_dir -ChildPath "modules\shell-fu"
+makeDir($modules_home)
 copy .\shell-fu.psm1 $modules_home\shell-fu.psm1
 copy .\shell-fu.psd1 $modules_home\shell-fu.psd1
